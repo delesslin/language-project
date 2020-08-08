66b0568601dabb13e247ef8f98c166979546502c
delesslin <delesslingw@gmail.com>
2020-08-08 11:41:08 -0400

    :construction: adding build file back

---
+++ app/build/precache-manifest.417aa76b1e65483e33d81dd1e6bcbe0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66b4765d0ee1d3a6ede67ad6d8ab6fbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c4799fb7243449e3d8e",
+    "url": "/static/js/2.a123b27d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a123b27d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff0b575ac32aabdfc8ff",
+    "url": "/static/js/main.6a707e98.chunk.js"
+  },
+  {
+    "revision": "3f809adccd7b5eb81ce7",
+    "url": "/static/js/runtime-main.6d8ceafa.js"
+  }
+]);
