c3b0f40853d1cfd6f8559bd05b9ffe8c7cec2cc3
delesslin <delesslingw@gmail.com>
2020-08-15 17:20:22 -0400

    Fixed server/db integration

--- server/index.js
+++ server/index.js
@@ -4,15 +4,15 @@ const PORT = process.env.PORT || 3333
 const express = require('express')
 const { serveBuildFolder } = require('./serveBuildFolder')
 const { apiRouter } = require('./apiRouter')
-const mongoose = require("mongoose")
-const { MongoClient } = require("mongodb")
+const mongoose = require('mongoose')
+const { MongoClient } = require('mongodb')
 
 // Init express server
 const app = express()
 
-//mongoDB Atlas 
+//mongoDB Atlas
 const CONNECTION_URL = process.env.CONNECTION_URL
-console.log("----------------------------------------")
+console.log('----------------------------------------')
 console.log(CONNECTION_URL)
 // Handle API requests
 
@@ -23,40 +23,36 @@ serveBuildFolder(app, '/../app/build')
 
 // const client = new MongoClient(CONNECTION_URL)
 
+async function listDatabases(client) {
+  databasesList = await client.db().admin().listDatabases()
+
+  console.log('Databases:')
+  databasesList.databases.forEach((db) => console.log(` - ${db.name}`))
+}
 // Start server
 async function main() {
   /**
    * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
    * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
    */
-  const client = new MongoClient(CONNECTION_URL, { useUnifiedTopology: true });
+  const client = new MongoClient(CONNECTION_URL, { useUnifiedTopology: true })
 
   try {
     // Connect to the MongoDB cluster
-    await client.connect();
+    await client.connect()
+    await listDatabases(client)
     app.listen(PORT, async (e) => {
       try {
         if (e) throw e
         console.log(`Server listening on http://localhost:${PORT}`)
-        await listDatabases(client);
-      }
-      catch (e) {
+      } catch (e) {
         console.error(e)
       }
     })
-
-
   } catch (e) {
-    console.error(e);
+    console.error(e)
   } finally {
-    await client.close();
+    await client.close()
   }
 }
-async function listDatabases(client) {
-  databasesList = await client.db().admin().listDatabases();
-
-  console.log("Databases:");
-  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
-main().catch(console.error);
-
+main().catch(console.error)
