dd4912921f4dc188104aeff60d761299bf5dc5d2
delesslin <delesslingw@gmail.com>
2020-08-15 22:50:48 -0400

    :construction: adding scaffold for App word input form

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,21 @@
 import React, { useState } from 'react'
 import logo from './logo.svg'
+import Header from './Header'
 import './App.css'
+import { Paper, Button, TextField } from '@material-ui/core'
 // TODO: Implement React Router
 // TODO: Admin page
 // TODO: Public page
+
 function App() {
-  const [data, setData] = useState('cat')
   return (
     <div className='App'>
-      <h1>Hello World! TEST</h1>
-      <h3>We're learnign react</h3>
-      <h4>{data}</h4>
+      <Header />
+
+      <Paper>
+        <TextField varient='filled' />
+        <Button onClick={() => console.log('clicked')}>Submit</Button>
+      </Paper>
     </div>
   )
 }
