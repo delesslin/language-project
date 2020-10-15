96910c2ce676f4c61890962a9279ba17bd3a3101
delesslin <delesslingw@gmail.com>
2020-10-15 19:32:48 -0400

    :rocket: fixed directory issue in server.js

--- server/serveBuildFolder.js
+++ server/serveBuildFolder.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const serveBuildFolder = (expressApp, relativePath) => {
   expressApp.use(express.static(path.join(__dirname, relativePath)))
+  // expressApp.use(express.static(relativePath))
   // Handles any requests that don't match the ones above
   expressApp.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + `${relativePath}/index.html`))
