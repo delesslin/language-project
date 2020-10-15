c0e322b412cc534e47853f3edf24a78486652451
delesslin <delesslingw@gmail.com>
2020-10-15 19:37:40 -0400

    :rocket: removing build folder from .gitignore

---
+++ app/build/precache-manifest.e073391ce998df0e8ad239265913e753.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49d1d5db349dab718874906f633182a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1677867d713cf7e389b",
+    "url": "/static/js/2.0d03fe8e.chunk.js"
+  },
+  {
+    "revision": "8d033527bfde269db1e0fb1aca3c57e8",
+    "url": "/static/js/2.0d03fe8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24ed14a2a8b2ce20b0a4",
+    "url": "/static/js/main.6cd6e0f1.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  }
+]);
