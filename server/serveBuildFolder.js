2a4e89d93ba013a287f2d496ea6fc2580c2e4d4d
delesslin <delesslingw@gmail.com>
2020-10-15 19:35:20 -0400

    :rocket: fixing serve issue

--- server/serveBuildFolder.js
+++ server/serveBuildFolder.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const serveBuildFolder = (expressApp, relativePath) => {
-  expressApp.use(express.static(path.join(__dirname, relativePath)))
+  expressApp.use(express.static('./app/build'))
   // expressApp.use(express.static(relativePath))
   // Handles any requests that don't match the ones above
   expressApp.get('*', (req, res) => {
