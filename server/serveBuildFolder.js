5786286092f11a90a0750095bc09f25afc674fce
delesslin <delesslingw@gmail.com>
2020-10-16 13:41:55 -0400

    :construction: attempting to fix express catch all

--- server/serveBuildFolder.js
+++ server/serveBuildFolder.js
@@ -4,7 +4,7 @@ const serveBuildFolder = (expressApp, relativePath) => {
   expressApp.use(express.static('./app/build'))
   // expressApp.use(express.static(relativePath))
   // Handles any requests that don't match the ones above
-  expressApp.get('*', (req, res) => {
+  expressApp.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname + `${relativePath}/index.html`))
   })
 }
