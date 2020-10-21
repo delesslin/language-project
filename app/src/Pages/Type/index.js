d0846aa4c8f58ce8f3dcb5ed96c05a47b540ee6d
delesslin <delesslingw@gmail.com>
2020-10-20 21:19:28 -0400

    :wrench: refactoring Type

--- app/src/Pages/Type/index.js
+++ app/src/Pages/Type/index.js
@@ -1,35 +1,13 @@
-import { Grid, Typography } from '@material-ui/core'
 import React from 'react'
-import styled from 'styled-components'
+import { FaKeyboard } from 'react-icons/fa'
+import { Page } from '../../Components'
 import { CopyKeyboard } from '../../Components/Keyboard'
 
-const StyledGridContainer = styled(Grid)`
-  padding: 40px;
-  margin: 0px;
-`
 const Type = () => {
-  // const StyledGridContainer = styled(Grid)({
-  //   padding: '40px',
-  //   margin: '0px',
-  // })
-
   return (
-    <>
-      <StyledGridContainer container direction='column' alignItems='center'>
-        <Grid item>
-          <Typography variant='h4'>
-            Type In Catawba{' '}
-            <span role='img' aria-label='keyboard emoji'>
-              ⌨
-            </span>
-          </Typography>
-        </Grid>
-        <Grid>
-          <CopyKeyboard />
-          {/* <AddKeyboard /> */}
-        </Grid>
-      </StyledGridContainer>
-    </>
+    <Page Icon={FaKeyboard}>
+      <CopyKeyboard />
+    </Page>
   )
 }
 
