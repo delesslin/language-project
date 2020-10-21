52ce9493e716c92e46312e87fca4fb76abcda4f0
delesslin <delesslingw@gmail.com>
2020-10-20 20:10:14 -0400

    :wrench: hiding admin button on smaller than desktop

--- app/src/Components/Layout/Footer/index.js
+++ app/src/Components/Layout/Footer/index.js
@@ -45,10 +45,8 @@ const AdminButton = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
-  ${media('<tablet')} {
-    justify-content: center;
-  }
-  ${media('<tablet')} {
+
+  ${media('<desktop')} {
     display: none;
   }
 `
