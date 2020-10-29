1d1252d6e3878ce2e561b05354ade86c160ab835
delesslin <delesslingw@gmail.com>
2020-10-29 19:02:55 -0400

    :wrench: ugh

---
+++ app/build/precache-manifest.43a8763088303b90dcd246ea2332d134.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19e522cfe179a75119c71188f2155b6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bf775b75e23cbe7b865",
+    "url": "/static/js/2.32185357.chunk.js"
+  },
+  {
+    "revision": "280b2cf68e9e5bb980b2be8717ff80f2",
+    "url": "/static/js/2.32185357.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96dd5976a12ba7ad1d05",
+    "url": "/static/js/main.00da0d72.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  }
+]);
